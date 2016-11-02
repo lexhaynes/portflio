858601257e0283e7ded7ddaa4acd0941c44421db
Alex Haynes <alex@Alex.local>
2016-11-02 06:15:42 -0400

    added fix-to-top banners

--- script.js
+++ script.js
@@ -1,40 +1,56 @@
-/*var $ = document.querySelectorAll.bind(document);
-var gridItems = $('.grid-item');*/
+var $ = document.querySelectorAll.bind(document);
+var gridItems = $('.grid-item');
 
 
-/*Object.keys(gridItems).map(function(key, index) {
+Object.keys(gridItems).map(function(key, index) {
 
-	gridItems[key].addEventListener('mouseenter', function() { toggleDivAndNearest(this, index) });	
-	gridItems[key].addEventListener('mouseout', function() { toggleDivAndNearest(this, index) });
-});*/
+	gridItems[key].addEventListener('mouseenter', function() { 
+		console.log('enter ', index);
+		toggleDivAndNearest(this, index), false });	
+	gridItems[key].addEventListener('mouseleave', function() { toggleDivAndNearest(this, index), false });
+});
 
-$('.grid-item').mouseenter(function() {
-	$(this).toggleClass('expanded');
-	console.log($(this).index());
-	if ($(this).index() % 2 !=0 ) {
-		$(this).prev().toggleClass('contracted');
-	} else {
-		$(this).next().toggleClass('contracted');
-	}
-}).mouseleave(function() {
-	$(this).toggleClass('expanded');
 
-	if ($(this).index() % 2 !=0 ) {
-		$(this).prev().toggleClass('contracted');
-	} else {
-		$(this).next().toggleClass('contracted');
-	}
-})
-
-/*function toggleDivAndNearest(div, index) {
+function toggleDivAndNearest(div, index) {
 	div.classList.toggle('expanded');
 
-	if (index % 2 !=0 ) {
-		div.previousElementSibling.classList.toggle('contracted')
+	if (index % 2 ==0 ) {
+		div.nextElementSibling.classList.toggle('contracted');
 	} else {
-		div.nextElementSibling.classList.toggle('contracted')
+		div.previousElementSibling.classList.toggle('contracted');
 	}
-}*/
+}
 
 
+var about = document.getElementById('about-banner');
+var contact = document.getElementById('contact-banner');
+
+
+window.addEventListener('scroll', function() {
+	console.log(this.scrollY);
+	var banner1Appears = 970;
+	var banner2Appears = 3000;
+	
+	var container1Height = about.parentElement.clientHeight;
+	var banner1Gone = banner1Appears+container1Height;
+
+	var container2Height = contact.parentElement.clientHeight;
+	var banner2Gone = banner2Appears+container2Height;
+
+	if (this.scrollY > banner1Appears) {
+		about.classList.add('fixed-top');
+	}
+	if (this.scrollY > banner1Gone || this.scrollY < banner1Appears ) {
+		about.classList.remove('fixed-top');
+	}	
+
+	if (this.scrollY > banner2Appears) {
+		contact.classList.add('fixed-top');
+	}
+	if (this.scrollY > banner2Gone || this.scrollY < banner2Appears ) {
+		contact.classList.remove('fixed-top');
+	}
+
+	 	
+})
 
